1d5f3c65ccd39224cdb068c2d0bdbcd32e6c9601
Jeremy Hansen <jhansen1344@gmail.com>
2020-03-01 08:27:50 -0500

    data layers setup

---
+++ ActiviList.Data/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviList.Data
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Too many characters")]
+        [Display(Name="Item Name")]
+        public string Name { get; set; }
+        [Display(Name = "Item Location")]
+        public string Location { get; set; }
+
+    }
+}
---
+++ ActiviList.Data/SubActivity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiviList.Data
+{
+    public class SubActivity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Item> Items { get; set; }
+
+    }
+}
